4f0f0deef94c1f451b83d4f079dc8f43afd274d4
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2021-08-06 01:27:38 +0530

    Basic if statements

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let firstCard = 6;
+let firstCard = 13;
 let secondCard = 8;
-
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-console.log(sum);
+function startGame(){
+    if (sum <= 20){
+        message = "DO you want to draw a new card?";
+    } else if (sum === 21){
+        message = "Woohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game";
+        isAlive = false;
+    }
+    console.log(message);
+}
+
